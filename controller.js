50abd207a7f5a3bf265761198acfc8b5e7132258
DragonRider970901 <109847967+DragonRider970901@users.noreply.github.com>
2022-11-22 20:13:10 +0200

    Add files via upload

---
+++ controller.js
@@ -0,0 +1,24 @@
+var onrow = 0;
+
+if(onrow<0)
+{
+    const recpimgContainer = document.createElement("td");
+    /*const recipeimage = document.querySelector("#recipe_img");
+    var uploaded_img = "";
+    image_input.addEventListener("change", function(){
+        const reader = new FileReader();
+        reader.addEventListener("load", ()=> {
+            uploaded_img = reader.result;
+            document.querySelector("#")
+        })
+    })*/
+}
+
+function addTabelElement()
+{
+    var tabel = document.getElementById("#recipe_tabel");
+    if(onrow<0)
+    {
+        var cell=row.insertCell();
+    }
+}
